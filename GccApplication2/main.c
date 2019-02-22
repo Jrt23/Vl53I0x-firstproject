191fb02702fc0af6d2b076aeba4d104689948a25
Jorik <Joriktuinfort@gmail.com>
2019-02-22 21:22:19 +0100

    init the project

---
+++ GccApplication2/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication2.c
+ *
+ * Created: 22-2-2019 20:00:56
+ * Author : Remco
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
